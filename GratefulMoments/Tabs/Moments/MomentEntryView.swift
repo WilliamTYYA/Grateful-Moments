778e5bcb0128da1b54e8ef61de2a6f62fab02f3f
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2025-11-18 12:16:19 -0600

    add MomentEntryView

---
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -0,0 +1,44 @@
+//
+//  MomentEntryView.swift
+//  GratefulMoments
+//
+//  Created by Thiha Ye Yint Aung on 11/18/25.
+//
+
+import SwiftUI
+
+struct MomentEntryView: View {
+    @State private var title = ""
+    @State private var note = ""
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                contentStack
+            }
+            .scrollDismissesKeyboard(.interactively)
+            .navigationTitle("Grateful For")
+        }
+    }
+    
+    var contentStack: some View {
+        VStack(alignment: .leading) {
+            TextField(text: $title) {
+                Text("Title (Required)")
+            }
+            .font(.title.bold())
+            .padding(.top, 48)
+            
+            Divider()
+            
+            TextField("Log your small wins", text: $note, axis: .vertical)
+                .multilineTextAlignment(.leading)
+                .lineLimit(5...Int.max)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MomentEntryView()
+}
