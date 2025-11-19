5ad9e6a699457acee3691ec0799a52d70d8ce1bf
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2025-11-19 00:14:41 -0600

    Badge model and BadgeDetailView

---
+++ GratefulMoments/Models/Badge.swift
@@ -0,0 +1,25 @@
+//
+//  Badge.swift
+//  GratefulMoments
+//
+//  Created by Thiha Ye Yint Aung on 11/19/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Badge {
+    var details: BadgeDetails
+    
+    init(details: BadgeDetails) {
+        self.details = details
+    }
+}
+
+extension Badge {
+    static var sample: Badge {
+        let badge = Badge(details: .firstEntry)
+        return badge
+    }
+}
