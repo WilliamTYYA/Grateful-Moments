6484efa434cea761a34ac98a25e44844cf254d1a
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2025-11-19 01:27:43 -0600

    BadgeManager

---
+++ GratefulMoments/Models/BadgeManager.swift
@@ -0,0 +1,31 @@
+//
+//  BadgeManager.swift
+//  GratefulMoments
+//
+//  Created by Thiha Ye Yint Aung on 11/19/25.
+//
+
+import Foundation
+import SwiftData
+
+class BadgeManager {
+    private let modelContainer: ModelContainer
+    
+    init(modelContainer: ModelContainer) {
+        self.modelContainer = modelContainer
+    }
+    
+    func loadBadgesIfNeeded() throws {
+        let context = modelContainer.mainContext
+        
+        var fetchDescriptor = FetchDescriptor<Badge>()
+        fetchDescriptor.fetchLimit = 1
+        let existingBadges = try context.fetch(fetchDescriptor)
+        
+        if existingBadges.isEmpty {
+            for details in BadgeDetails.allCases {
+                context.insert(Badge(details: details))
+            }
+        }
+    }
+}
